e42ce44223a15192cefb9f890d756c2584f1bace
Michelle Enkhjargal <98449219+mlnkhx@users.noreply.github.com>
2022-03-30 14:26:52 +0800

    Update moviecinema.py

--- moviecinema.py
+++ moviecinema.py
@@ -1,1 +1,18 @@
+name = input("Welcome! What is you name?")
+print("Hello,", name +"!")
+number = input("What is your number?")
 
+def t_movie():
+    print("Which movie would you like to watch today", name +"?")
+    print("1,Dune")
+    print("2,The Batman")
+    print("3,Tick Tick Boom")
+    print("4,Don't Look Up")
+    movie = int(input("Please choose your movie: "))
+
+def theater():
+    print("Which session would you like to book?")
+    print("Morning")
+    print("Afternoon")
+    print("Evening")
+    a = int(input("Please choose your session you'd like to book:"))
